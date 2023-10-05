a5e949f18291059dcaae73f4447db7d7158aca3d
Bernat <bernat13@gmail.com>
2023-10-05 13:38:35 +0200

    detalle partido

--- src/app/interfaces/ipartido.ts
+++ src/app/interfaces/ipartido.ts
@@ -1,2 +1,13 @@
+import { IJugada } from "./ijugada";
+
 export interface IPartido {
+    id:Number,
+    fecha:Date,
+    equipo_v:String,
+    equipo_l:String,
+    marcador_v:Number,
+    marcador_l:Number,
+    logo_l:String,
+    logo_v:String,
+    jugadas: IJugada[]
 }
